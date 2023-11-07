5ba7226bebbbe37b59c419f8d4581246e7854b03
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-11-07 22:43:10 +0300

    add

---
+++ EventAndFunctions.js
@@ -0,0 +1,15 @@
+var name = "özlem belörenoğlu"
+function showName(){
+    console.log(name);
+}
+
+function sum(num1, num2){
+    console.log(num1 + num2);
+}
+function writeText(text){
+    console.log("write metodu çalıştı");
+    document.write(text);
+}
+function bodyOnLoad(){
+    console.log("body yüklemesi tamamlandı");
+}
